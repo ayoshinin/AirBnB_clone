8e8b122e97eb3c150b20595ea3e14403c25e734d
ayoshinin <ayodejisamson73@yahoo.com>
2024-03-09 12:50:41 -0800

    Airbnb

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Unittest for BaseModel.'''
+
+
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    '''TestBaseModel tests all aspects of BaseModels.'''
+
+    def test_class(self):
+        '''Method test for ...'''
+        self.assertEqual()
+        
+
